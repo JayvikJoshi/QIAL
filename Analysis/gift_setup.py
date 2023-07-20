e8a012d7d55dff14bce8a8787980dea4d6695ff5
Jayvik Joshi <jayvik.joshi@gmail.com>
2023-07-20 17:59:04 -0400

    fix

--- Analysis/gift_setup.py
+++ Analysis/gift_setup.py
@@ -4,7 +4,7 @@ in_dir = r"C:\Users\jayvi\Desktop\QIAL\GIFT Testing\input\APOE4_4HN\\"
 for file in os.listdir(in_dir):
     filepath = in_dir + file
     if file.endswith(".nii.gz"):
-        os.system(f"gzip.exe {filepath}")
+        os.system(f"gzip.exe -d {filepath}")
 
     if file.endswith(".nii"):
         out_dir = in_dir + "APOE4_" + file[0:9] + "\\"
